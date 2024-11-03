a2ad23983eb9cf5768b419b693f9f5870cd8a647
yurio24 <lestermillaresadang@gmail.com>
2024-11-03 09:44:26 +0800

    First commit

---
+++ script.js
@@ -0,0 +1,8 @@
+ // Typed.js Initialization
+        var typing = new Typed(".text", {
+            strings: ["a Web Developer", "a Web Designer", "a Freelancer", "a Graphics Designer"],
+            typeSpeed: 100,
+            backSpeed: 60,
+            loop: true,
+        });
+
